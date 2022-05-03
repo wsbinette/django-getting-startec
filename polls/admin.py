8d1b43f922c4233f09a6386f1e269ecd091cecc4
wsbinette <wsbinette@icloud.com>
2022-05-03 15:56:29 -0400

    safety upload

--- polls/admin.py
+++ polls/admin.py
@@ -7,11 +7,19 @@ from .models import Question, Choice
 # class QuestionAdmin(admin.ModelAdmin):
 #     fields = ['pub_date', 'question_text']
 
+class ChoiceInLine(admin.TabularInline):
+	model = Choice
+	extra = 3
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,               {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date']}),
     ]
+    inlines = [ChoiceInLine]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,5 @@
 import datetime
+from django.contrib import admin
 
 from django.db import models
 from django.utils import timezone
@@ -9,6 +10,11 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
     def __str__(self):
         return self.question_text
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Was the question published recently?'
+    )
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
